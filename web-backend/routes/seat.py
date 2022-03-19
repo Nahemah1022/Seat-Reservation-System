49044926524e31cc142122e66b439f36e783ac4b
bencyc1129 <bencyc1129@gmail.com>
2022-03-19 19:51:37 +0800

    [Update]book route

--- web-backend/routes/seat.py
+++ web-backend/routes/seat.py
@@ -6,8 +6,12 @@ from schemas import seatSchema
 seatRouter = APIRouter()
 
 @seatRouter.post("/book")
-def bookSeat(studentId: str, seatId: int):
-    print(studentId, seatId)
+def bookSeat(s: seatSchema.dbSeat):
+    print(s.seat_id, s.reserved_by)
+    # asume that the request is always valid.
+    # we don't handle the situation that the user trys to book the reserved seat.
+    # if the seat is reserved or unclickable, the user won't access this route.
+    conn.execute(t_Seat.insert().values(s.dict()))
     return "success"
 
 @seatRouter.post("/cancel")
