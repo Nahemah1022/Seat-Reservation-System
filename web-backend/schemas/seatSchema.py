fd85d086163a9f7fbe038114f676a99b5b80ce19
bencyc1129 <bencyc1129@gmail.com>
2022-03-19 20:48:17 +0800

    [Update] change course_id type to str

--- web-backend/schemas/seatSchema.py
+++ web-backend/schemas/seatSchema.py
@@ -2,7 +2,7 @@ import datetime
 from pydantic import BaseModel
 
 class dbSeat(BaseModel):
-    course_id: int
+    course_id: str
     course_date: datetime.date
     seat_id: int
     reserved_by: str
