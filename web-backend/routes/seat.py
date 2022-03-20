f2e7a83668c141c07ef44eeb63a0d7913c5aff75
bencyc1129 <bencyc1129@gmail.com>
2022-03-20 21:07:26 +0800

    [Update] revise the seat book/cancel api's params format

--- web-backend/routes/seat.py
+++ web-backend/routes/seat.py
@@ -8,18 +8,26 @@ from schemas import seatSchema
 seatRouter = APIRouter()
 
 @seatRouter.post("/book")
-def bookSeat(s: seatSchema.dbSeat):
+def bookSeat(course_id : str, course_date : datetime.date, seat_id : int, reserved_by : str):
     # asume that the request is always valid.
     # we don't handle the situation that the user trys to book the reserved seat.
     # if the seat is reserved or unclickable, the user won't access this route.
-    conn.execute(t_Seat.insert().values(s.dict()))
+    conn.execute(t_Seat.insert().values(
+        {
+            "course_id" : course_id,
+            "course_date" : course_date,
+            "seat_id" : seat_id,
+            "reserved_by" : reserved_by
+        }
+    ))
     return "success"
 
 @seatRouter.post("/cancel")
-def cancelBookedSeat(s: seatSchema.dbSeat):
-    print(s)
+def cancelBookedSeat(course_id : str, course_date : datetime.date, seat_id : int, reserved_by : str):
     conn.execute(t_Seat.delete().where(
-        t_Seat.c.reserved_by == s.reserved_by,
-        t_Seat.c.seat_id == s.seat_id
+        t_Seat.c.course_id == course_id,
+        t_Seat.c.course_date == course_date,
+        t_Seat.c.seat_id == seat_id,
+        t_Seat.c.reserved_by == reserved_by
         ))
     return "success"
