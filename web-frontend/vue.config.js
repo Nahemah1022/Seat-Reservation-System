baaec97c7cc972a6a3806dfa13dc3da9d22facc4
haoyu0970624763 <haoyu0970624763@gmail.com>
2022-04-02 00:34:29 +0800

    [Setting] Fix proxy problem

--- web-frontend/vue.config.js
+++ web-frontend/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: "http://140.116.249.231:8000",
+  },
 });
